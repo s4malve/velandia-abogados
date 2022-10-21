8c0525278d4ad47bbeefe21524ee0876425adaf1
s4malve <s4malve@gmail.com>
2022-10-21 17:18:33 -0500

    fix: change button data

--- src/data/button.ts
+++ src/data/button.ts
@@ -1,8 +1,10 @@
 export const variants = {
-  primary: 'bg-primary shadow-lg shadow-primary/40 hover:bg-primary/90'
+  primary:
+    'bg-primary shadow-lg shadow-primary/40 hover:bg-primary/90 active:hover:bg-primary/80'
 }
 
 export const sizes = {
-  sm: '',
-  md: ''
+  sm: 'px-6 text-sm',
+  md: 'px-6 text-base',
+  lg: 'px-14'
 }
