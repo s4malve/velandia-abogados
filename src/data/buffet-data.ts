6876373d300e92ec2c390ab26dbe2590f1072637
s4malve <s4malve@gmail.com>
2022-11-16 17:37:22 -0500

    feat: add buffet data

---
+++ src/data/buffet-data.ts
@@ -0,0 +1,32 @@
+export default [
+  {
+    content: '+103 Casos ganados en estos 18 años de experiencia',
+    className: 'col-start-1 col-end-5',
+    active: true
+  },
+  {
+    content: '+103 Casos ganados en estos 18 años de experiencia',
+    className: 'col-start-5 col-end-8',
+    active: false
+  },
+  {
+    content: '+103 Casos ganados en estos 18 años de experiencia',
+    className: 'col-start-8 col-end-11',
+    active: false
+  },
+  {
+    content: '+103 Casos ganados en estos 18 años de experiencia',
+    className: 'col-start-1 col-end-4 grid-rows-2',
+    active: false
+  },
+  {
+    content: '+103 Casos ganados en estos 18 años de experiencia',
+    className: 'col-start-4 col-end-7 grid-rows-2',
+    active: false
+  },
+  {
+    content: '+103 Casos ganados en estos 18 años de experiencia',
+    className: 'col-start-7 col-end-11 grid-rows-2',
+    active: false
+  }
+]
