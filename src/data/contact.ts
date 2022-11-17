2a27223c877b3a0a90cf88f28f323082e15c254d
s4malve <s4malve@gmail.com>
2022-11-17 12:18:12 -0500

    feat: add contact data

---
+++ src/data/contact.ts
@@ -0,0 +1,21 @@
+import Whatsapp from '@/components/icons/Whatsapp.astro'
+import Gmail from '@/components/icons/Gmail.astro'
+import Location from '@/components/icons/Location.astro'
+
+export default [
+  {
+    icon: Whatsapp,
+    content: '(+57) 320 378 9546',
+    name: 'whatsapp'
+  },
+  {
+    icon: Gmail,
+    content: 'yovese@gmail.com',
+    name: 'gmail'
+  },
+  {
+    icon: Location,
+    content: 'Cra 19 # 6 - 28',
+    name: 'location'
+  }
+]
