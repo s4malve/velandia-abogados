1f0de8b343f046413f2de77d7a915dbee0a599a9
s4malve <s4malve@gmail.com>
2022-11-17 12:18:20 -0500

    feat: add social data

---
+++ src/data/social.ts
@@ -0,0 +1,24 @@
+import Facebook from '@/components/icons/Facebook.astro'
+import Twitter from '@/components/icons/Twitter.astro'
+import Linkedin from '@/components/icons/Linkedin.astro'
+
+export default [
+  {
+    icon: Facebook,
+    content: 'Facebook',
+    name: 'facebook',
+    to: ''
+  },
+  {
+    icon: Twitter,
+    content: 'Twitter',
+    name: 'twitter',
+    to: ''
+  },
+  {
+    icon: Linkedin,
+    content: 'Linkedin',
+    name: 'linkedin',
+    to: ''
+  }
+]
