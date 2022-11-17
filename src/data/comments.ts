d900bc6d4d30f00a52ec56c6c552e2145456f593
s4malve <s4malve@gmail.com>
2022-11-17 11:13:00 -0500

    feat: add comments data

---
+++ src/data/comments.ts
@@ -0,0 +1,11 @@
+export default [
+  {
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est',
+    author: {
+      name: 'Marta Gutierrez',
+      position: 'CEO',
+      company: 'Vaqueritos'
+    }
+  }
+]
