7a774d12f79d1908935542540bea373b113f062d
s4malve <s4malve@gmail.com>
2022-11-17 16:27:46 -0500

    feat: add example footer links data

---
+++ src/data/footer.ts
@@ -0,0 +1,56 @@
+export default [
+  {
+    title: 'Areas',
+    to: '',
+    items: [
+      {
+        title: 'Familia',
+        to: ''
+      },
+      {
+        title: 'Civil',
+        to: ''
+      },
+      {
+        title: 'Administrativo',
+        to: ''
+      }
+    ]
+  },
+  {
+    title: 'Sobre Nosotros',
+    to: '',
+    items: [
+      {
+        title: 'Familia',
+        to: ''
+      },
+      {
+        title: 'Civil',
+        to: ''
+      },
+      {
+        title: 'Administrativo',
+        to: ''
+      }
+    ]
+  },
+  {
+    title: 'Contacto',
+    to: '',
+    items: [
+      {
+        title: 'Familia',
+        to: ''
+      },
+      {
+        title: 'Civil',
+        to: ''
+      },
+      {
+        title: 'Administrativo',
+        to: ''
+      }
+    ]
+  }
+]
