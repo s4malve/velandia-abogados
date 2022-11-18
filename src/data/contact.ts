46c9574f680d16188c94a7ef19e7b694bd642ab3
s4malve <s4malve@gmail.com>
2022-11-17 19:31:42 -0500

    feat: add path to contacts data

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -6,16 +6,19 @@ export default [
   {
     icon: Whatsapp,
     content: '(+57) 320 378 9546',
+    to: 'https://api.whatsapp.com/send?phone=573203789546&text=Hola%20Luz...',
     name: 'whatsapp'
   },
   {
     icon: Gmail,
     content: 'yovese@gmail.com',
+    to: null,
     name: 'gmail'
   },
   {
     icon: Location,
     content: 'Cra 19 # 6 - 28',
+    to: 'https://g.page/LuzVelandiaAbogados?share',
     name: 'location'
   }
 ]
