30351229dc995814c988c6e4497dcd6b256f7975
s4malve <s4malve@gmail.com>
2022-11-17 19:32:06 -0500

    feat: add new paths to nav data

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -2,5 +2,13 @@ export const links = [
   {
     name: 'Inicio',
     path: '/'
+  },
+  {
+    name: 'Sobre Nosotros',
+    path: '#sobre-nosotros'
+  },
+  {
+    name: 'Contactanos',
+    path: '#contactanos'
   }
 ]
