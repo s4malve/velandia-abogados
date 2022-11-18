b65415bc8c90569bf6643dfa70f941cec2b350fe
s4malve <s4malve@gmail.com>
2022-11-18 16:45:59 -0500

    feat: set astro output as server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react()]
-});
+  output: 'server',
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }),
+    react()
+  ]
+})
