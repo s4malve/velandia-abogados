1b62cdafa6d79729135b01c647ad34ef97d3c89c
s4malve <s4malve@gmail.com>
2022-11-22 15:12:36 -0500

    feat: add variants to button
    secondary, success, loading and error

--- src/data/button.ts
+++ src/data/button.ts
@@ -1,10 +1,17 @@
 export const variants = {
   primary:
-    'bg-primary text-slate-100 shadow-lg shadow-primary/40 hover:bg-primary/90 active:hover:bg-primary/80'
+    'bg-primary text-slate-100 shadow-lg shadow-primary/40 hover:bg-primary/90 active:hover:bg-primary/80',
+  secondary:
+    'bg-slate-100 enabled:hover:bg-slate-200 enabled:active:bg-slate-300 text-slate-900',
+  success:
+    'bg-green-400 enabled:hover:bg-green-500 enabled:active:bg-green-600',
+  loading:
+    'bg-indigo-400 enabled:hover:bg-indigo-500 enabled:active:bg-indigo-600',
+  error: 'bg-rose-400 enabled:hover:bg-rose-500 enabled:active:bg-rose-600'
 }
 
 export const sizes = {
   sm: 'px-6 text-sm',
-  md: 'px-6 text-base',
-  lg: 'px-14'
+  md: 'px-6 py-2 text-base',
+  lg: 'px-6 py-3'
 }
