573ce03c68e074a26826f79c4a270d8290f7cc32
s4malve <s4malve@gmail.com>
2022-11-22 17:43:33 -0500

    fix: remove state

--- src/api/sendMessage.ts
+++ src/api/sendMessage.ts
@@ -1,38 +1,9 @@
 import type TContactForm from '@/types/contactForm'
 
-import { useState } from 'react'
-
-type MessageStatus = {
-  loading: boolean
-  data: string
-  error?: string
-}
-
 export default async function sendMessage(contactForm: TContactForm) {
-  const [status, setStatus] = useState<MessageStatus>({
-    data: '',
-    error: undefined,
-    loading: false
+  return await fetch('/send-message', {
+    method: 'POST',
+    body: JSON.stringify(contactForm),
+    headers: { 'Content-Type': 'application/json' }
   })
-  setStatus((prev) => ({ ...prev, error: undefined, loading: true }))
-  try {
-    const res = await fetch('/send-message', {
-      method: 'POST',
-      body: JSON.stringify(contactForm),
-      headers: { 'Content-Type': 'application/json' }
-    })
-
-    if (res.ok) {
-      const data = await res.json()
-      setStatus((prev) => ({ ...prev, data }))
-    }
-
-    await Promise.reject(res)
-  } catch (error) {
-    setStatus((prev) => ({ ...prev, error: error as string }))
-  } finally {
-    setStatus((prev) => ({ ...prev, loading: false }))
-  }
-
-  return status
 }
