cd0d8da5ed970ecfb1f7c6b466c7b13df64e1329
s4malve <s4malve@gmail.com>
2022-11-22 20:32:11 -0500

    fix: remove comments of sendMessage api

--- src/api/sendMessage.ts
+++ src/api/sendMessage.ts
@@ -3,7 +3,7 @@ import type TContactForm from '@/types/contactForm'
 export default function sendMessage(contactForm: TContactForm) {
   return fetch('/send-message', {
     method: 'POST',
-    body: JSON.stringify(contactForm)
-    // headers: { 'Content-Type': 'application/json' }
+    body: JSON.stringify(contactForm),
+    headers: { 'Content-Type': 'application/json' }
   })
 }
